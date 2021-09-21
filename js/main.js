a1707bcb0613b1b5729e19e4f1ce4f2228bfab96
Adilton Sgit <adiltons28@gmail.com>
2021-09-21 19:09:16 -0300

    drag and drop update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 import {renderFromLocal, deleteFromLocal, saveOnLocal, updateOnLocal} from "../js/localStorageFunctions.js";
 
+const dragArea = document.querySelector(".ul-todo");
+new Sortable(dragArea, {
+    animation: 100
+});
+
 let noncompledtasks = 0;
 
 function Task(title){
